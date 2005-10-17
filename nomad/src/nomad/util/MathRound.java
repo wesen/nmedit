c0bf0cd4b5c8a2f796a2c68d725062de35b166ba
chresan <chresan>
2005-10-17 21:29:33 +0000

    *** empty log message ***

--- nomad/src/nomad/util/MathRound.java
+++ nomad/src/nomad/util/MathRound.java
@@ -3,7 +3,12 @@ package nomad.util;
 public final class MathRound {
 
 	/**
-	 * rounds until digit '-to'
+	 * rounds d until digit '-to'. For example d=123, to=2 returns 120.
+	 * If d=123.456 and to=-2 then 123.45 is returned.
+	 * 
+	 * @param d Value to round
+	 * @param to decimal position
+	 * @return rounded value of d
 	 */
 	public static double round(double d, int to) {
 		if (to == 0)
@@ -13,6 +18,12 @@ public final class MathRound {
 		return Math.round(d*norm) / norm;
 	}
 	
+	/**
+	 * Returns the value of d as a String. If d is a natural number,
+	 * it is returned without decimal point.
+	 * @param d value that should be converted to a string representation
+	 * @return d as string
+	 */
 	public static String doubleToStr(double d) {
 		double truncated = (int) d;
 		if (d==truncated)
