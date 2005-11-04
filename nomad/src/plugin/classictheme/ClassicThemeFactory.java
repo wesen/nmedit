111b428f7532471fe478d8da3b2b6c0b86ae5bb3
chresan <chresan>
2005-11-04 16:08:15 +0000

    updated themes + new theme

--- nomad/src/plugin/classictheme/ClassicThemeFactory.java
+++ nomad/src/plugin/classictheme/ClassicThemeFactory.java
@@ -1,13 +1,21 @@
 package plugin.classictheme;
 
+import java.awt.Graphics;
+
+import nomad.graphics.BackgroundPainter;
+import nomad.gui.ModuleSectionGUI;
 import nomad.gui.model.UIFactory;
 import nomad.gui.model.component.builtin.ButtonGroupUI;
 import nomad.gui.model.component.builtin.DefaultConnectorUI;
 import nomad.gui.model.component.builtin.DefaultControlUI;
 import nomad.gui.model.component.builtin.DefaultLabelUI;
 import nomad.gui.model.component.builtin.DefaultTextDisplay;
+import nomad.gui.model.component.builtin.SliderArrayUI;
+import nomad.gui.model.component.builtin.SliderArrayUI14;
 import nomad.gui.model.component.builtin.SliderUI;
 import nomad.gui.model.component.builtin.VocoderUI;
+import nomad.misc.ImageTracker;
+import nomad.patch.ModuleSection;
 
 public class ClassicThemeFactory extends UIFactory {
 	public ClassicThemeFactory() {
@@ -18,6 +26,10 @@ public class ClassicThemeFactory extends UIFactory {
 		installUIClass(ButtonGroupUI.class);
 		installUIClass(SliderUI.class);
 		installUIClass(VocoderUI.class);
+		installUIClass(SliderArrayUI.class);
+		installUIClass(SliderArrayUI14.class);
+		
+		DefaultControlUI.resetKnobLook();
 		
 		installDefaultControl(DefaultControlUI.class);
 		installDefaultOptionControl(ButtonGroupUI.class);
@@ -28,4 +40,28 @@ public class ClassicThemeFactory extends UIFactory {
 	public String getUIDescriptionFileName() {
 		return "src/plugin/classictheme/ui.xml";
 	}
+
+	public ModuleSectionGUI getModuleSectionGUI(ModuleSection moduleSection) {
+		return new ClassicModuleSectionGUI(moduleSection, getImageTracker());
+	}
+	
+}
+
+class ClassicModuleSectionGUI extends ModuleSectionGUI {
+	private static BackgroundPainter bgpainter = null;
+	
+	public ClassicModuleSectionGUI(ModuleSection moduleSection, ImageTracker itracker) {
+		super(moduleSection) ;
+		
+		if (bgpainter==null) {
+			bgpainter = new BackgroundPainter(
+					this, itracker.getImage("classic-patch-background")
+				);
+		}
+	}
+
+	public void paintComponent(Graphics g) {
+		bgpainter.paintBackground(g);
+	}
+	
 }
