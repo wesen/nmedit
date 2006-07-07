0657079dcd8f561fc74467fc7434f1add4f58c70
chresan <chresan>
2006-07-07 04:48:07 +0000

    themes are now loaded with a service provider

---
+++ nomad/src/net/sf/nmedit/nomad/theme/plugin/ThemePluginManager.java
@@ -0,0 +1,82 @@
+package net.sf.nmedit.nomad.theme.plugin;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import sun.misc.Service;
+
+import net.sf.nmedit.nomad.theme.UIFactory;
+
+
+/**
+ * The plugin manager.
+ * 
+ * @author Christian Schneider
+ */
+public class ThemePluginManager 
+{
+    
+    private static ThemePluginProvider[] themes = null;
+    
+    public static ThemePluginProvider[] getThemes()
+    {
+        if (themes != null) return themes;
+        
+        List<ThemePluginProvider> list = new ArrayList<ThemePluginProvider>();
+        Iterator i = Service.providers(ThemePluginProvider.class);
+        while (i.hasNext())
+        {
+            ThemePluginProvider p = (ThemePluginProvider) i.next();
+            list.add(p);
+        }
+        
+        return themes = list.toArray(new ThemePluginProvider[list.size()]);
+    }
+    
+	/**
+	 * Returns the number of available plugins
+	 * @return the number of available plugins
+	 */
+	public static int getPluginCount() 
+    {
+		return getThemes().length;
+	}
+	
+	/**
+	 * Returns a plugin at the given index.
+	 * @param index the index
+	 * @return the plugin
+	 */
+	public static ThemePluginProvider getPlugin(int index) 
+    {
+		return getThemes()[index];
+	}
+    
+    public static ThemePluginProvider getDefaultProvider()
+    {
+        return getProvider("Classic Theme");
+    }
+    
+    public static ThemePluginProvider getProvider(String name)
+    {
+        for (ThemePluginProvider p : getThemes())
+        {
+            if (p.getName().equals(name))
+                return p;
+        }
+        
+        return null;
+    }
+	
+	/**
+	 * Returns the default UIFactory instance.
+	 * @return the default UIFactory instance.
+	 */
+	public static UIFactory getDefaultUIFactory() 
+    {
+        ThemePluginProvider p = getDefaultProvider();
+        return p.getFactory();
+	}
+	
+}
---
+++ nomad/src/net/sf/nmedit/nomad/theme/plugin/ThemePluginProvider.java
@@ -0,0 +1,57 @@
+/* Copyright (C) 2006 Christian Schneider
+ * 
+ * This file is part of Nomad.
+ * 
+ * Nomad is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * Nomad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Nomad; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/*
+ * Created on Jul 7, 2006
+ */
+package net.sf.nmedit.nomad.theme.plugin;
+
+import net.sf.nmedit.nomad.theme.UIFactory;
+
+public abstract class ThemePluginProvider
+{
+
+    /**
+     * Returns the name of the plugin.
+     * @return the name of the plugin.
+     */
+    public abstract String getName();
+    
+    /**
+     * Returns an array containing the author names.
+     * @return array containing the author names.
+     */
+    public abstract String[] getAuthors();
+    
+    /**
+     * Returns an description on the plugin.
+     * @return an description on the plugin.
+     */
+    public abstract String getDescription();
+
+    /**
+     * Returns an instance of the factory. 
+     * @return instance of the factory.
+     */
+    public abstract UIFactory getFactory();
+
+    public abstract String getHomepage();
+    public abstract String getVersion();
+    
+}
