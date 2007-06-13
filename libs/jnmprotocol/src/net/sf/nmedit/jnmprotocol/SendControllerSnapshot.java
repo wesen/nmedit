711fbcce740f9b1079477d2a4861a23f1d519356
chresan <chresan>
2007-06-13 06:19:59 +0000

    send controller snapshot added (not confirmed as working, yet)

---
+++ libs/jnmprotocol/src/net/sf/nmedit/jnmprotocol/SendControllerSnapshot.java
@@ -0,0 +1,63 @@
+/*
+    Nord Modular Midi Protocol 3.03 Library
+    Copyright (C) 2003-2006 Marcus Andersson
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package net.sf.nmedit.jnmprotocol;
+
+import java.util.*;
+import net.sf.nmedit.jpdl.*;
+
+public class SendControllerSnapshot extends MidiMessage
+{
+    public SendControllerSnapshot()
+    {
+	super();
+
+	addParameter("pid", "data:pid");
+	addParameter("sc", "data:sc");
+	set("cc", 0x17);
+	set("sc", 0x55);
+    }
+
+    SendControllerSnapshot(Packet packet)
+    {
+	this();
+	setAll(packet); // this direction is not supported
+    }
+
+    public void setSource(int slot, int pid)
+    {
+        set("slot", slot);
+        set("pid", pid);
+    }
+    
+    public List getBitStream()
+	throws Exception
+    {
+	IntStream intStream = appendAll();
+	LinkedList bitStreamList = new LinkedList();
+	bitStreamList.add(getBitStream(intStream));
+	return bitStreamList;
+    }
+
+    @Override
+    public void notifyListener(NmProtocolListener listener) throws Exception
+    {
+        // this message can only be sent to the synth, not to the editor 
+    }
+}
