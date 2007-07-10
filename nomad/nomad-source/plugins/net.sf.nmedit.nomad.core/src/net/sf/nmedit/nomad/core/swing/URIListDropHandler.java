ed37bf5740fb4330b700228999475dd18460108c
chresan <chresan>
2007-07-10 12:21:46 +0000

    + dropping files into Nomad now supported

---
+++ nomad/nomad-source/plugins/net.sf.nmedit.nomad.core/src/net/sf/nmedit/nomad/core/swing/URIListDropHandler.java
@@ -0,0 +1,127 @@
+package net.sf.nmedit.nomad.core.swing;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class URIListDropHandler implements DropTargetListener
+{
+
+    public static final String MIME_TEXT_URI_LIST = "text/uri-list";
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    
+    public boolean isDFSupported(DataFlavor flavor)
+    {
+        return String.class.equals(flavor.getRepresentationClass())
+        && (
+                flavor.isMimeTypeEqual(MIME_TEXT_URI_LIST)
+                || flavor.isMimeTypeEqual(MIME_TEXT_PLAIN)
+        );
+    }
+    
+    public DataFlavor getSupportedFlavor(DataFlavor[] list)
+    {
+        DataFlavor plainFlavor = null;
+        for (DataFlavor flavor: list)
+        {   
+            if (String.class.equals(flavor.getRepresentationClass()))
+            {
+                // prefer URI List
+                if (flavor.isMimeTypeEqual(MIME_TEXT_URI_LIST))
+                    return flavor;
+                else if (flavor.isMimeTypeEqual(MIME_TEXT_PLAIN))
+                    plainFlavor = flavor;
+            }
+        }
+     
+        return plainFlavor;
+    }
+    
+    public void checkEvent(DropTargetDragEvent dtde)
+    {
+        if (getSupportedFlavor(dtde.getCurrentDataFlavors())!=null)
+            dtde.acceptDrag(DnDConstants.ACTION_COPY_OR_MOVE);
+        else
+            dtde.rejectDrag();
+    }
+    
+    public void dragEnter(DropTargetDragEvent dtde)
+    {
+        checkEvent(dtde);
+    }
+
+    public void dropActionChanged(DropTargetDragEvent dtde)
+    {
+        checkEvent(dtde);
+    }
+    
+    public void dragOver(DropTargetDragEvent dtde)
+    {
+        // no op
+    }
+
+    public void dragExit(DropTargetEvent dte)
+    {
+        // no op
+    }
+
+    public void drop(DropTargetDropEvent dtde)
+    {
+        DataFlavor flavor = getSupportedFlavor(dtde.getCurrentDataFlavors());
+        if (flavor == null)
+        {
+            dtde.rejectDrop();
+            return;
+        }
+
+        dtde.acceptDrop(dtde.getDropAction());
+        List<URI> uris;
+        try
+        {
+            Reader reader = flavor.getReaderForText(dtde.getTransferable());
+
+            BufferedReader lineReader = new BufferedReader(reader);
+            uris = new ArrayList<URI>();
+            String line;
+            while ((line=lineReader.readLine())!=null)
+            {
+                try
+                {
+                    uris.add(new URI(line));
+                }
+                catch (URISyntaxException e)
+                {
+                    // not a URI => unknown data => abort
+                    return;
+                }   
+            }
+        }
+        catch (UnsupportedFlavorException e1)
+        {
+            throw new RuntimeException(e1);
+        }
+        catch (IOException e1)
+        {
+            throw new RuntimeException(e1);
+        }
+        if (uris.size()>0)
+            uriListDropped(uris.toArray(new URI[uris.size()]));
+    }
+
+    public void uriListDropped(URI[] uriList)
+    {
+        // no op
+    }
+    
+}
