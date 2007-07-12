61d007e3e0e47b0d252d1beebad9a7ec37fa8edc
chresan <chresan>
2007-07-12 17:10:39 +0000

    +fixed bug: midi dialog did not select the previous input/output

--- nomad/nomad-source/plugins/net.sf.nmedit.nomad.core/src/net/sf/nmedit/nomad/core/forms/NomadMidiDialogFrm.java
+++ nomad/nomad-source/plugins/net.sf.nmedit.nomad.core/src/net/sf/nmedit/nomad/core/forms/NomadMidiDialogFrm.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2006 Christian Schneider
+/* Copyright (C) 2007 Christian Schneider
  * 
  * This file is part of Nomad.
  * 
@@ -25,30 +25,21 @@ import java.awt.Container;
 import java.awt.Dimension;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 
 
 public class NomadMidiDialogFrm extends JPanel
 {
-   /**
-     * 
-     */
-    private static final long serialVersionUID = -8229919657319024068L;
-JComboBox m_cbOutDevices = new JComboBox();
-   JLabel m_lblInVendor = new JLabel();
-   JLabel m_lblInVersion = new JLabel();
-   JLabel m_lblOutVendor = new JLabel();
-   JComboBox m_cbInDevices = new JComboBox();
-   JLabel m_lblOutDescription = new JLabel();
-   JLabel m_lblInDescription = new JLabel();
-   JLabel m_lblOutVersion = new JLabel();
-   JButton m_btnRefresh = new JButton();
-   JSeparator m_jseparator1 = new JSeparator();
-   JSeparator m_jseparator2 = new JSeparator();
+   JLabel lblInVendor = new JLabel();
+   JLabel lblInVersion = new JLabel();
+   JLabel lblOutVendor = new JLabel();
+   JLabel lblOutDescription = new JLabel();
+   JLabel lblInDescription = new JLabel();
+   JLabel lblOutVersion = new JLabel();
+   JComboBox cbOutDevices = new JComboBox();
+   JComboBox cbInDevices = new JComboBox();
 
    /**
     * Default constructor
@@ -128,90 +119,73 @@ JComboBox m_cbOutDevices = new JComboBox();
    public JPanel createPanel()
    {
       JPanel jpanel1 = new JPanel();
-      FormLayout formlayout1 = new FormLayout("FILL:DEFAULT:NONE,FILL:DEFAULT:NONE,FILL:DEFAULT:GROW(1.0)","CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE");
+      FormLayout formlayout1 = new FormLayout("FILL:DEFAULT:NONE,FILL:8DLU:NONE,FILL:DEFAULT:GROW(1.0)","CENTER:DEFAULT:NONE,CENTER:4DLU:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:4DLU:NONE,CENTER:DEFAULT:NONE,CENTER:4DLU:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE,CENTER:DEFAULT:NONE");
       CellConstraints cc = new CellConstraints();
       jpanel1.setLayout(formlayout1);
 
-      m_cbOutDevices.setName("cbOutDevices");
-      jpanel1.add(m_cbOutDevices,cc.xy(3,9));
-
       JLabel jlabel1 = new JLabel();
       jlabel1.setText("Vendor");
-      jpanel1.add(jlabel1,cc.xy(1,5));
+      jpanel1.add(jlabel1,cc.xy(1,4));
 
       JLabel jlabel2 = new JLabel();
       jlabel2.setText("Version");
-      jpanel1.add(jlabel2,cc.xy(1,6));
+      jpanel1.add(jlabel2,cc.xy(1,5));
 
       JLabel jlabel3 = new JLabel();
       jlabel3.setText("Vendor");
-      jpanel1.add(jlabel3,cc.xy(1,12));
+      jpanel1.add(jlabel3,cc.xy(1,10));
 
       JLabel jlabel4 = new JLabel();
       jlabel4.setText("Version");
-      jpanel1.add(jlabel4,cc.xy(1,13));
+      jpanel1.add(jlabel4,cc.xy(1,11));
 
-      m_lblInVendor.setName("lblInVendor");
-      m_lblInVendor.setText("lblInVendor");
-      jpanel1.add(m_lblInVendor,cc.xy(3,5));
+      lblInVendor.setName("lblInVendor");
+      lblInVendor.setText("lblInVendor");
+      jpanel1.add(lblInVendor,cc.xy(3,4));
 
-      m_lblInVersion.setName("lblInVersion");
-      m_lblInVersion.setText("lblInVersion");
-      jpanel1.add(m_lblInVersion,cc.xy(3,6));
+      lblInVersion.setName("lblInVersion");
+      lblInVersion.setText("lblInVersion");
+      jpanel1.add(lblInVersion,cc.xy(3,5));
 
-      m_lblOutVendor.setName("lblOutVendor");
-      m_lblOutVendor.setText("lblOutVendor");
-      jpanel1.add(m_lblOutVendor,cc.xy(3,12));
+      lblOutVendor.setName("lblOutVendor");
+      lblOutVendor.setText("lblOutVendor");
+      jpanel1.add(lblOutVendor,cc.xy(3,10));
+
+      lblOutDescription.setName("lblOutDescription");
+      lblOutDescription.setText("lblOutDescription");
+      jpanel1.add(lblOutDescription,cc.xy(3,9));
 
       JLabel jlabel5 = new JLabel();
-      jlabel5.setText("Device");
-      jpanel1.add(jlabel5,cc.xy(1,2));
-
-      m_cbInDevices.setName("cbInDevices");
-      jpanel1.add(m_cbInDevices,cc.xy(3,2));
-
-      m_lblOutDescription.setName("lblOutDescription");
-      m_lblOutDescription.setText("lblOutDescription");
-      jpanel1.add(m_lblOutDescription,cc.xy(3,11));
+      jlabel5.setText("Description");
+      jpanel1.add(jlabel5,cc.xy(1,9));
 
       JLabel jlabel6 = new JLabel();
       jlabel6.setText("Description");
-      jpanel1.add(jlabel6,cc.xy(1,11));
+      jpanel1.add(jlabel6,cc.xy(1,3));
+
+      lblInDescription.setName("lblInDescription");
+      lblInDescription.setText("lblInDescription");
+      jpanel1.add(lblInDescription,cc.xy(3,3));
+
+      lblOutVersion.setName("lblOutVersion");
+      lblOutVersion.setText("lblOutVersion");
+      jpanel1.add(lblOutVersion,cc.xy(3,11));
 
       JLabel jlabel7 = new JLabel();
-      jlabel7.setText("Description");
-      jpanel1.add(jlabel7,cc.xy(1,4));
-
-      m_lblInDescription.setName("lblInDescription");
-      m_lblInDescription.setText("lblInDescription");
-      jpanel1.add(m_lblInDescription,cc.xy(3,4));
+      jlabel7.setText("Output");
+      jpanel1.add(jlabel7,cc.xy(1,7));
 
       JLabel jlabel8 = new JLabel();
-      jlabel8.setText("Device");
-      jpanel1.add(jlabel8,cc.xy(1,9));
+      jlabel8.setText("Input");
+      jpanel1.add(jlabel8,cc.xy(1,1));
 
-      m_lblOutVersion.setName("lblOutVersion");
-      m_lblOutVersion.setText("lblOutVersion");
-      jpanel1.add(m_lblOutVersion,cc.xy(3,13));
+      cbOutDevices.setName("cbOutDevices");
+      jpanel1.add(cbOutDevices,cc.xy(3,7));
 
-      JLabel jlabel9 = new JLabel();
-      jlabel9.setText("Output");
-      jpanel1.add(jlabel9,cc.xy(1,8));
+      cbInDevices.setName("cbInDevices");
+      jpanel1.add(cbInDevices,cc.xy(3,1));
 
-      JLabel jlabel10 = new JLabel();
-      jlabel10.setText("Input");
-      jpanel1.add(jlabel10,cc.xy(1,1));
-
-      m_btnRefresh.setActionCommand("Refresh");
-      m_btnRefresh.setName("btnRefresh");
-      m_btnRefresh.setText("Refresh");
-      jpanel1.add(m_btnRefresh,new CellConstraints(3,15,1,1,CellConstraints.RIGHT,CellConstraints.DEFAULT));
-
-      jpanel1.add(m_jseparator1,cc.xy(3,1));
-
-      jpanel1.add(m_jseparator2,cc.xy(3,8));
-
-      addFillComponents(jpanel1,new int[]{ 2 },new int[]{ 3,7,10,14,15,16 });
+      addFillComponents(jpanel1,new int[]{ 2 },new int[]{ 2,6,8 });
       return jpanel1;
    }
 
