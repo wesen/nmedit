8abbb1d9bc4fed9d95cf810d9050d5dffb3a6500
chresan <chresan>
2008-02-04 14:17:22 +0000

    +root==null case not possible anymore

--- libs/jpatch/src/net/sf/nmedit/jpatch/impl/PBasicConnectionManager.java
+++ libs/jpatch/src/net/sf/nmedit/jpatch/impl/PBasicConnectionManager.java
@@ -475,8 +475,6 @@ public class PBasicConnectionManager implements PConnectionManager
     public PConnector output(PConnector c)
     {
         PConnector root = root(c);
-        if (root == null)
-        	return null;
         return root.isOutput() ? root : null;
     }
 
