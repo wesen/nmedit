bd51faffd8598591d6b1c30a7e3f0093d43d14fb
m-odendahl <wesen@ruinwesen.com>
2008-02-09 11:22:21 +0000

    Added recursive directory delete

--- libs/nmutils/src/net/sf/nmedit/nmutils/io/FileUtils.java
+++ libs/nmutils/src/net/sf/nmedit/nmutils/io/FileUtils.java
@@ -59,4 +59,19 @@ public class FileUtils {
 	public static  void copyFile(File src, File dst) throws IOException {
 		copy(src, dst);
 	}
+	
+	static public boolean deleteDirectory(File path) {
+		if( path.exists() ) {
+			File[] files = path.listFiles();
+			for(int i=0; i<files.length; i++) {
+				if(files[i].isDirectory()) {
+					deleteDirectory(files[i]);
+				}
+				else {
+					files[i].delete();
+				}
+			}
+		}
+		return( path.delete() );
+	}
 }
