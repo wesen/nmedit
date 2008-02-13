7816dff735307a970ccd7a95607b0ce4a1234cd3
m-odendahl <wesen@ruinwesen.com>
2008-02-13 14:49:01 +0000

    Add cleaner workaround for SwingUtilities bug

---
+++ libs/jtheme/src/net/sf/nmedit/jtheme/util/JThemeUtils.java
@@ -0,0 +1,64 @@
+package net.sf.nmedit.jtheme.util;
+
+import java.awt.Component;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+
+import javax.swing.SwingUtilities;
+import javax.swing.event.MenuDragMouseEvent;
+
+public class JThemeUtils {
+	public static MouseEvent convertMouseEvent(Component source,
+			MouseEvent sourceEvent,
+			Component destination) {
+		Point p = SwingUtilities.convertPoint(source,new Point(sourceEvent.getX(),
+				sourceEvent.getY()),
+				destination);
+		Component newSource;
+
+		if(destination != null)
+			newSource = destination;
+		else
+			newSource = source;
+
+		MouseEvent newEvent;
+		if (sourceEvent instanceof MouseWheelEvent) {
+			MouseWheelEvent sourceWheelEvent = (MouseWheelEvent)sourceEvent;
+			newEvent = new MouseWheelEvent(newSource,
+					sourceWheelEvent.getID(),
+					sourceWheelEvent.getWhen(),
+					sourceWheelEvent.getModifiersEx(),
+					p.x,p.y,
+					sourceWheelEvent.getClickCount(),
+					sourceWheelEvent.isPopupTrigger(),
+					sourceWheelEvent.getScrollType(),
+					sourceWheelEvent.getScrollAmount(),
+					sourceWheelEvent.getWheelRotation());
+		}
+		else if (sourceEvent instanceof MenuDragMouseEvent) {
+			MenuDragMouseEvent sourceMenuDragEvent = (MenuDragMouseEvent)sourceEvent;
+			newEvent = new MenuDragMouseEvent(newSource,
+					sourceMenuDragEvent.getID(),
+					sourceMenuDragEvent.getWhen(),
+					sourceMenuDragEvent.getModifiersEx(),
+					p.x,p.y,
+					sourceMenuDragEvent.getClickCount(),
+					sourceMenuDragEvent.isPopupTrigger(),
+					sourceMenuDragEvent.getPath(),
+					sourceMenuDragEvent.getMenuSelectionManager());
+		}
+		else {
+			newEvent = new MouseEvent(newSource,
+					sourceEvent.getID(),
+					sourceEvent.getWhen(),
+					sourceEvent.getModifiersEx(),
+					p.x,p.y,
+					sourceEvent.getClickCount(),
+					sourceEvent.isPopupTrigger());
+		}
+		return newEvent;
+	}
+
+
+}
