7a7f89762edf2adf976935638456c625559442ae
chresan <chresan>
2008-02-19 20:27:58 +0000

    + new javadoc comments + removed break statement

--- libs/jpdl2/src/net/sf/nmedit/jpdl2/PDLItem.java
+++ libs/jpdl2/src/net/sf/nmedit/jpdl2/PDLItem.java
@@ -18,100 +18,125 @@
 */
 package net.sf.nmedit.jpdl2;
 
+import net.sf.nmedit.jpdl2.stream.BitStream;
+import net.sf.nmedit.jpdl2.stream.IntStream;
+
 public interface PDLItem 
 {
     
     /**
-     * Returns the minimum size of this item.
-     * @return the minimum size of this item
+     * Returns the minimum number of bits required to parse this item (and it's children).
      * 
-     * TODO ensure that no infinite recursion occures 
+     * The return value is used by the packet parser to estimate the number of required
+     * bits in the {@link BitStream BitStream}.
+     * 
+     * @return the minimum size of this item and it's children 
      */
     int getMinimumSize();
 
-    // number of values, TODO documentation
+    /**
+     * Returns the minimum number of values required to parse this item (and it's children).
+     * 
+     * The return value is used by the packet parser to estimate the number of required
+     * values in the {@link IntStream IntStream}.
+     * 
+     * @return the minimum number of values of this item and it's children
+     */
     int getMinimumCount();
     
     /**
-     * Returns the type of this item 
+     * Returns the {@link PDLItemType type} of this item.
      * @return the type of this item
      */
     PDLItemType getType();
     
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to work.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLLabel asLabel();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLConstant asConstant();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLImplicitVariable asImplicitVariable();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLVariable asVariable();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLVariableList asVariableList();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLPacketRef asPacketRef();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLPacketRefList asPacketRefList();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLConditional asConditional();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLOptional asOptional();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLMutualExclusion asMutualExclusion();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLBlockItem asBlock();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLInstruction asInstruction();
 
     /**
      * Casts this item to the desired item type.
+     * Casting this item directly to the desired item type is not guaranteed to be successful.
      * @throws ClassCastException if the item is not of the desired type
      */
     PDLSwitchStatement asSwitchStatement();
