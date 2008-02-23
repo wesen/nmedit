ee60f630972f96102aed51f951ba46f34e60a5cd
chresan <chresan>
2008-02-23 12:07:06 +0000

    +respect instecs +paint string support

--- libs/nordmodular/src/net/sf/nmedit/jtheme/clavia/nordmodular/misc/GradientProgressBar.java
+++ libs/nordmodular/src/net/sf/nmedit/jtheme/clavia/nordmodular/misc/GradientProgressBar.java
@@ -26,9 +26,18 @@ import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Insets;
 import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
 
+import javax.swing.JComponent;
 import javax.swing.JProgressBar;
+import javax.swing.plaf.ProgressBarUI;
+import javax.swing.plaf.basic.BasicProgressBarUI;
+
+import sun.swing.SwingUtilities2;
 
 public class GradientProgressBar extends JProgressBar
 {
@@ -45,6 +54,19 @@ public class GradientProgressBar extends JProgressBar
     private int o = getOrientation();
     
     private GradientPaint gp = null;
+    
+    public GradientProgressBar()
+    {
+        super();
+        
+        setUI(new GradientProgressBarUI());
+    }
+    
+    public void setUI(ProgressBarUI ui) {
+        if (ui == null || (!(ui instanceof GradientProgressBarUI)))
+            return;
+        super.setUI(ui);
+    }
     
     public void setGradient(Color c)
     {
@@ -66,12 +88,12 @@ public class GradientProgressBar extends JProgressBar
     {
         return gradient;
     }
-    
-    protected void paintComponent(Graphics g)
+    /*
+    protected void paintGradient(Graphics g)
     {
         Color a = getForeground();
         if (a==null || gradient == null || a.equals(gradient))
-            super.paintComponent(g);
+            return;
         
         Color b = gradient;
         
@@ -88,7 +110,7 @@ public class GradientProgressBar extends JProgressBar
             else
                 gp = new GradientPaint(0, 0, b, 0, getHeight(), a, false);
         }
-        
+
         g2.setPaint(getBackground());
         g2.fillRect(0, 0, w, h);
         
@@ -102,5 +124,66 @@ public class GradientProgressBar extends JProgressBar
         
         g2.setPaint(oldp);
     }
+    */
+    private static class GradientProgressBarUI extends BasicProgressBarUI
+    {
+        protected void paintDeterminate(Graphics g, JComponent c) 
+        {
+            if (!(g instanceof Graphics2D)) {
+                return;
+            }
+            GradientProgressBar gpb = (GradientProgressBar) c;
 
+            Insets b = progressBar.getInsets(); // area for border
+            int barRectWidth = progressBar.getWidth() - (b.right + b.left);
+            int barRectHeight = progressBar.getHeight() - (b.top + b.bottom);
+            int amountFull = getAmountFull(b, barRectWidth, barRectHeight);
+                
+                if (barRectWidth <= 0 || barRectHeight <= 0) {
+                    return;
+                }
+
+                Graphics2D g2 = (Graphics2D)g;
+
+                    Color ca = gpb.getForeground();
+                    Color cb = gpb.gradient;
+                    
+                    Paint oldp = g2.getPaint();
+             
+                    if (gpb.gp == null || barRectWidth!=gpb.w || barRectHeight!=gpb.h || gpb.o!= gpb.getOrientation())
+                    {
+                        gpb.w = barRectWidth;
+                        gpb.h = barRectHeight;
+                        gpb.o = gpb.getOrientation();
+                        if (gpb.o==HORIZONTAL)
+                            gpb.gp = new GradientPaint(0, 0, ca, barRectWidth, 0, cb, false);
+                        else
+                            gpb.gp = new GradientPaint(0, 0, cb, 0, barRectHeight, ca, false);
+                    }
+
+                    g2.setPaint(gpb.getBackground());
+                    g2.fillRect(0, 0, gpb.w, gpb.h);
+                    
+                    g2.setPaint(gpb.gp);
+                    
+                    float p = gpb.getValue()/((float)gpb.getMaximum()-gpb.getMinimum());
+                    if (gpb.o==HORIZONTAL)
+                        g2.fillRect(b.left, b.top, (int)(barRectWidth*p), barRectHeight);
+                    else
+                        g2.fillRect(b.left, b.top, barRectWidth, (int)(barRectHeight*p));
+                    
+                    g2.setPaint(oldp);
+                    
+
+            // Deal with possible text painting
+                    // Deal with possible text painting
+                    if (progressBar.isStringPainted()) {
+                        
+                        paintString(g, b.left, b.top,
+                            barRectWidth, barRectHeight,
+                            amountFull, b);
+                    }
+        }
+    }
+    
 }
