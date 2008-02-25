9b939cb208f0d6e3ae97d2c1f792aed1711eac58
chresan <chresan>
2008-02-25 10:52:05 +0000

    +javadoc comments

--- libs/jpatch/src/net/sf/nmedit/jpatch/PRoles.java
+++ libs/jpatch/src/net/sf/nmedit/jpatch/PRoles.java
@@ -25,16 +25,42 @@ import java.util.Collection;
 public interface PRoles extends Collection<String>, Iterable<String>
 {
 
+    /**
+     * Returns true if the set is empty.
+     */
     boolean isEmpty();
     
+    /**
+     * Returns the number of strings in this set.
+     */
     int size();
     
+    /**
+     * Computes the union of this and the specified set.
+     * @param roles another set
+     * @return the union of this and the specified set
+     */
     PRoles union(PRoles roles);
 
+    /**
+     * Computes the difference of this and the specified set.
+     * @param roles another set
+     * @return the difference of this and the specified set
+     */
     PRoles difference(PRoles roles);
-    
+
+    /**
+     * Computes the intersection of this and the specified set.
+     * @param roles another set
+     * @return the intersection of this and the specified set
+     */
     PRoles intersection(PRoles roles);
 
+    /**
+     * Returns true if this and the specified set intersect.
+     * @param roles another set
+     * @return true if this and the specified set intersect
+     */
     boolean intersects(PRoles roles);
     
 }
