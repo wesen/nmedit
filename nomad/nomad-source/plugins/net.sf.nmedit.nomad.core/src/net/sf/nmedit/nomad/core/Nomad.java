70abf75d7235c22f87b348c2d76a7744d5f4ccd9
chresan <chresan>
2008-02-25 10:36:15 +0000

    +moved SelectedAction class in own file

--- nomad/nomad-source/plugins/net.sf.nmedit.nomad.core/src/net/sf/nmedit/nomad/core/Nomad.java
+++ nomad/nomad-source/plugins/net.sf.nmedit.nomad.core/src/net/sf/nmedit/nomad/core/Nomad.java
@@ -34,8 +34,6 @@ import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.dnd.DropTarget;
 import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.RenderedImage;
@@ -49,9 +47,7 @@ import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
-import javax.swing.AbstractButton;
 import javax.swing.Action;
-import javax.swing.ButtonGroup;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -86,6 +82,7 @@ import net.sf.nmedit.nomad.core.service.initService.InitService;
 import net.sf.nmedit.nomad.core.swing.ButtonBarBuilder;
 import net.sf.nmedit.nomad.core.swing.Factory;
 import net.sf.nmedit.nomad.core.swing.JDropDownButtonControl;
+import net.sf.nmedit.nomad.core.swing.SelectedAction;
 import net.sf.nmedit.nomad.core.swing.URIListDropHandler;
 import net.sf.nmedit.nomad.core.swing.document.DefaultDocumentManager;
 import net.sf.nmedit.nomad.core.swing.document.Document;
@@ -725,68 +722,6 @@ public class Nomad
     }
     
 
-    
-    private static class SelectedAction extends AbstractAction implements ItemListener
-    {
-        /**
-         * 
-         */
-        private static final long serialVersionUID = -2469667964205220429L;
-        private ButtonGroup bg;
-        private Action currentAction;
-
-        public SelectedAction()
-        {
-            this.bg = new ButtonGroup();
-            setEnabled(false);
-        }
-        
-        public void add(AbstractButton ab)
-        {
-            ab.addItemListener(this);
-            bg.add(ab);
-            setEnabled(true);
-            if (bg.getSelection() == null)
-                ab.setSelected(true);
-        }
-        
-        public void actionPerformed(ActionEvent e)
-        {
-            if (currentAction != null)
-                currentAction.actionPerformed(e);
-        }
-        
-        protected void setCurrentAction(Action o)
-        {
-            if (currentAction != o)
-            {
-                currentAction = o;
-                
-                Object actionCommand = null;
-                if (currentAction != null)
-                {
-                    actionCommand = currentAction.getValue(ACTION_COMMAND_KEY);
-                }
-                putValue(ACTION_COMMAND_KEY, actionCommand);
-            }
-            setEnabled(currentAction != null);
-        }
-        
-        protected Action getCurrentAction()
-        {
-            return currentAction;
-        }
-
-        public void itemStateChanged(ItemEvent e)
-        {
-            Object o = e.getSource();
-            if (o instanceof AbstractButton)
-                setCurrentAction(((AbstractButton)o).getAction());
-            else
-                setCurrentAction(null);
-        }
-        
-    }
     
     private static class AHAction extends AbstractAction
     {
