e69250a145d8a6ee5adeabd998e67450f6252560
m-odendahl <wesen@ruinwesen.com>
2008-03-05 22:22:08 +0000

    Return correct undo and redo names

--- libs/jpatch/src/net/sf/nmedit/jpatch/history/NamedCompoundEdit.java
+++ libs/jpatch/src/net/sf/nmedit/jpatch/history/NamedCompoundEdit.java
@@ -1,16 +1,43 @@
 package net.sf.nmedit.jpatch.history;
 
+import javax.swing.UIManager;
 import javax.swing.undo.CompoundEdit;
+import javax.swing.undo.UndoableEdit;
 
 public class NamedCompoundEdit extends CompoundEdit {
 	protected String presentationName;
-	
+
 	public NamedCompoundEdit(String name) {
 		super();
 		this.presentationName = name;
 	}
-	
+
 	public String getPresentationName() {
 		return presentationName;
 	}
+
+	public String getUndoPresentationName() {
+		String name = getPresentationName();
+		if (!"".equals(name)) {
+			name = UIManager.getString("AbstractUndoableEdit.undoText") +
+			" " + name;
+		} else {
+			name = UIManager.getString("AbstractUndoableEdit.undoText");
+		}
+
+		return name;
+	}
+
+	public String getRedoPresentationName() {
+		String name = getPresentationName();
+		if (!"".equals(name)) {
+			name = UIManager.getString("AbstractUndoableEdit.redoText") +
+			" " + name;
+		} else {
+			name = UIManager.getString("AbstractUndoableEdit.redoText");
+		}
+
+		return name;
+	}
+
 }
