8f5f7fd1463848acc3410344d6b219802fc4c6d9
chresan <chresan>
2008-03-05 00:21:00 +0000

    + named compoundedit

---
+++ libs/jpatch/src/net/sf/nmedit/jpatch/history/PUndoableEditSupport.java
@@ -0,0 +1,24 @@
+package net.sf.nmedit.jpatch.history;
+
+import javax.swing.undo.CompoundEdit;
+import javax.swing.undo.UndoableEditSupport;
+
+public class PUndoableEditSupport extends UndoableEditSupport
+{
+    public synchronized void beginUpdate(String name) 
+    {
+        if (updateLevel == 0) {
+            compoundEdit = createNamedCompoundEdit(name);
+        }
+        updateLevel++;
+    }
+    
+    /**
+     * Called only from <code>beginUpdate(String)</code>.
+     * Exposed here for subclasses' use.
+     */
+    protected CompoundEdit createNamedCompoundEdit(String name) 
+    {
+        return new CompoundEdit();
+    }
+}
